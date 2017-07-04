4886e7ae74225d983a65d4a92cdcd3613be21a87
Dave Iverson <dave@ivrsn.com>
2017-07-03 21:17:46 -0500

    added rochester library catalog search

--- frontend/src/BookSummary.js
+++ frontend/src/BookSummary.js
@@ -6,9 +6,17 @@ class BookSummary extends Component {
         return `${sum}, ${author.name}`;
       }, '')
         .slice(2);
+      // let encodedTitle = encodeURIComponent(this.props.book.title);
+      // let catalogUrl = `http://catalog.rochesterpubliclibrary.org/weblib/libcat?action=text-search&Ntt=${encodedTitle}&Ntk=Titles`;
+      let catalogTitleUrl = `https:\/\/ropl.ent.sirsi.net/client/default/search/results?dt=list&t%3Aformdata=&qu=${encodeURIComponent(this.props.book.title)}&rt=false%7C%7C%7CTITLE%7C%7C%7CTitle`;
+      let catalogAuthorUrl = `https:\/\/ropl.ent.sirsi.net/client/default/search/results?dt=list&t%3Aformdata=&qu=${encodeURIComponent(authors)}&rt=false%7C%7C%7CAUTHOR%7C%7C%7CAuthor`;
       return <div>
         <h2>{this.props.book.title}</h2>
         <span>{authors}</span>
+        <br />
+        <a href={catalogTitleUrl} target="_blank">Find title in Rochester Library</a>
+        <br />
+        <a href={catalogAuthorUrl} target="_blank">Find author in Rochester Library</a>
       </div>
     }
 }
