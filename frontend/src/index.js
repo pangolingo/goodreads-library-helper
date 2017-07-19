d0e5cefb28c5b3b48527d7be9046e3a8e0e31546
Dave Iverson <dave@ivrsn.com>
2017-07-19 16:36:31 -0500

    renabled service worker registration and rebuilt

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,4 +5,4 @@ import registerServiceWorker, { unregister } from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
