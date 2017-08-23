23b338d0ce5eb16edf9448407fccdcd6808982ed
David Iverson <diverson@DIVERSON.local>
2017-08-23 14:18:10 -0500

    removed frontend code from this repo: it's only for the API server. Also took the opportunity to clean and comment app.rb

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Reading List Library Helper
+# Reading List Library Helper - API Backend
 
 An app to list your Goodreads shelves and books, and cache them locally
 so you can browse them offline.
@@ -6,6 +6,10 @@ so you can browse them offline.
 This allows you to walk into the public library and quickly look through
 the books on your Goodreads to-read list, in order to find the ones you
 want to check out.
+
+## BACKEND
+
+This is the backend service. Because Goodreads uses OAuth 1.0, we need to store an API key and secret. We can't store that client-side, so we do the OAuth flow in the Ruby backend. All API calls to Goodreads must go through this server.
 
 ## Todo
 
--- app.rb
+++ app.rb
@@ -1,19 +1,8 @@
 require 'oauth'
 require 'dotenv/load'
-require 'mustermann'
 require 'sinatra'
 require "sinatra/reloader" if ENV['RACK_ENV'] == 'development'
-
-if ENV['RACK_ENV'] == 'production'
-  app_home_url = "/"
-  callback_url = "https://reading-list-library-helper.herokuapp.com/api/oauth/callback"
-elsif ENV['RACK_ENV'] == 'staging'
-  app_home_url = "/"
-  callback_url = "http://localhost:4567/api/oauth/callback"
-else
-  app_home_url = "http://localhost:3000/"
-  callback_url = "http://localhost:4567/api/oauth/callback"
-end
+require './guidestar'
 
 consumer = OAuth::Consumer.new(
   ENV['goodreads_api_key'],
@@ -26,13 +15,7 @@ set :server, :puma
 set :root, File.dirname(__FILE__)
 set :sessions, true
 set :static, true
-set :public_folder, Proc.new { File.join(settings.root, 'frontend', 'build') }
 
-auth_urls = Mustermann.new('/api/*', except: '/api/oauth*')
-before auth_urls do
-  puts 'Authenticating'
-  authenticate!
-end
 
 def authenticate!
   return if session[:access_token]
@@ -41,40 +24,73 @@ def authenticate!
   halt 401, {}, 'User not authenticated'
 end
 
-get '/' do
-  File.read(File.join(settings.root, 'frontend', 'build', 'index.html'))
+def access_token(consumer)
+  hash = {
+    oauth_token: session[:access_token],
+    oauth_token_secret: session[:access_token_secret]
+  }
+  access_token  = OAuth::AccessToken.from_hash(consumer, hash)
 end
 
-get '/api/oauth' do
 
-  request_token = consumer.get_request_token(:oauth_callback => callback_url)
 
+
+
+# ==============
+# ENDPOINTS
+# ==============
+
+# require authentication on all API urls
+before '/api/*' do
+  puts 'Authenticating'
+  authenticate!
+end
+
+get '/' do
+  'Reading List Library Helper - Backend API'
+end
+
+get '/oauth' do
+  client_callback_url = params[:callback_url]
+  server_callback_url = "#{request.base_url}/oauth/callback"
+
+  halt 400, {}, 'No callback url specified' unless client_callback_url
+
+  # build a request token object
+  request_token = consumer.get_request_token(:oauth_callback => server_callback_url)
+
+  # stash the token variables and redirect url to be used again in the callback
   session[:request_token] = request_token.token
   session[:request_token_secret] = request_token.secret
-  redirect request_token.authorize_url(:oauth_callback => callback_url)
+  session[:client_callback_url] = client_callback_url
 
+  # redirect to goodreads
+  redirect request_token.authorize_url(:oauth_callback => server_callback_url)
 end
 
-get '/api/oauth/callback' do
+get '/oauth/callback' do
+  # build a request token object
   hash = {
     oauth_token: session[:request_token],
     oauth_token_secret: session[:request_token_secret]
   }
   request_token  = OAuth::RequestToken.from_hash(consumer, hash)
 
-  # puts request_token
-  # puts consumer.access_token_url
+  # get an access token from the request token
   access_token = request_token.get_access_token
-  session[:access_token] = access_token.token
-  session[:access_token_secret] = access_token.secret
-  # # @photos = @access_token.get('/photos.xml')
-
   puts "i have an access token! #{access_token.token}"
 
+  # save the access token in the session for the user
+  session[:access_token] = access_token.token
+  session[:access_token_secret] = access_token.secret
 
-  # puts user.body
-  redirect app_home_url
+  # remove the request token and callback from the session
+  session.delete(:request_token)
+  session.delete(:request_token_secret)
+  client_callback_url = session.delete(:client_callback_url)
 
+  # redirect back to the API client
+  redirect client_callback_url
 end
 
 get '/api' do
@@ -90,54 +106,6 @@ end
 get '/api/shelves/:name' do
   g = Guidestar.new(access_token(consumer))
   g.shelf(params['name'], params['page'] || 1, params['per_page'] || 200)
-end
-
-# catchall
-get '/*' do
-  File.read(File.join(settings.root, 'frontend', 'build', 'index.html'))
-end
-
-def access_token(consumer)
-  hash = {
-    oauth_token: session[:access_token],
-    oauth_token_secret: session[:access_token_secret]
-  }
-  access_token  = OAuth::AccessToken.from_hash(consumer, hash)
-end
-
-class Guidestar
-
-  def initialize(access_token)
-    @access_token = access_token
-  end
-
-  def current_user
-    @access_token.get('/api/auth_user').body
-  end
-
-  def shelves
-    @access_token.get('/shelf/list').body
-  end
-
-  def shelf(name, page, per_page)
-    raise "per_page must be less than or equal to 200" if per_page.to_i > 200
-
-    url = '/review/list.xml'
-    query_string = Rack::Utils.build_query({
-      per_page: per_page,
-      page: page,
-      shelf: name,
-    })
-
-    # all guidestar options
-    #   # v: 2.to_s,
-    #   # # id: user_id.to_s,
-    #   # shelf: name,
-    #   # sort: 'title',
-    #   # per_page: 200.to_s,
-    #   # key: ENV['goodreads_api_key']
-    @access_token.get("#{url}?#{query_string}").body
-  end
 end
 
 
